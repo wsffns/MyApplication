06c475f6b082a68b779fcb38dd34f76ccba74b11
huangchl <1233>
2017-11-22 09:29:03 +0800

    访问网站的功能

--- app/src/main/java/com/example/huangchl/application1/MainActivity.java
+++ app/src/main/java/com/example/huangchl/application1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.huangchl.application1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goUrl(View view) {
+        EditText  teturl = (EditText)findViewById(R.id.teturl);
+        String Url = teturl.getText().toString();
+        Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(Url));
+        startActivity(intent);
     }
 }
